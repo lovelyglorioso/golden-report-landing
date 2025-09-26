640bf3cd1af71b8612b995021e047e9e4ac67301
lovelyglorioso <lovely@finpubmax.com>
2025-09-26 20:06:49 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",   // 👈 add this line
-
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/testing/",                 // <<--- IMPORTANT for /testing subfolder
+  server: { host: "::", port: 8080 },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 }));
